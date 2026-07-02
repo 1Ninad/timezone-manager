f12602e55a987f8c1ff38dec135de7caf6f7bba2
Ninad Kale <ninadkale200@gmail.com>
2026-07-02 10:49:39 +0530

    added AM/PM in table

--- wwwroot/js/timezone-interop.js
+++ wwwroot/js/timezone-interop.js
@@ -31,6 +31,7 @@ window.timezoneInterop = {
             day: 'numeric',
             hour: '2-digit',
             minute: '2-digit',
-            second: '2-digit'
+            second: '2-digit',
+            hour12: true
         }))
 };
